8cc086d1c3d2d46651c9f4d90b85754ef08105f4
koen van de velde <koenvandevelde@gmail.com>
2019-05-17 17:13:33 +0200

    added spring-boot-starter-web dep in order to get @RestController annotations

---
+++ src/main/java/com/vdvc/reactivedemo/Users/UserController.java
@@ -0,0 +1,19 @@
+package com.vdvc.reactivedemo.Users;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private static final String template = "Welcome, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/user")
+    public User user(@RequestParam(value="name", defaultValue="unknown user") String name) {
+        return new User(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
